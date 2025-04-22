ac1685961f447afbfe65df63e0dc0c73c05fbd32
MalaghePayal <malaghepayal@gmail.com>
2025-04-22 11:29:23 +0530

    [feat seed] - Seed Category table with initial demo data
    - Added seed data for Category entity using HasData in OnModelCreating
    - Created and applied migration to insert initial categories: Action, SciFi, History

--- MyntraWeb/Migrations/20250422054719_AddedRecordsInCcategoryTable.Designer.cs
+++ MyntraWeb/Migrations/20250422054719_AddedRecordsInCcategoryTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyntraWeb.Data;
 
@@ -10,9 +11,11 @@ using MyntraWeb.Data;
 namespace MyntraWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250422054719_AddedRecordsInCcategoryTable")]
+    partial class AddedRecordsInCcategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
