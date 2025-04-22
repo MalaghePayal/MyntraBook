86829b441919bd7018f725db38b3c667361334c9
MalaghePayal <malaghepayal@gmail.com>
2025-04-22 11:28:01 +0530

    [feat view] - - Implemented Index view to display categories in a table
    - Ordered categories by DisplayOrder

--- MyntraWeb/Controllers/CategoryController.cs
+++ MyntraWeb/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyntraWeb.Data;
+using MyntraWeb.Models;
 
 namespace MyntraWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _context.Categories.ToList();
+
+            return View(objCategoryList);
         }
     }
 }
