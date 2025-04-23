04152615644bdbdfb69eb0f98be80888bee4b224
MalaghePayal <malaghepayal@gmail.com>
2025-04-23 20:58:46 +0530

    [feat repository] - Add generic IRepository<T> interface for data access abstraction
    - Introduced IRepository<T> interface in Myntra.DataAccess.Repository.IRepository
    - Defined common CRUD operations: GetAll, Get, Add, Remove, RemoveRange
    - Facilitates reusable and consistent data access logic across entities

---
+++ Myntra.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myntra.DataAccess.Repository.IRepository
+{
+    internal interface IRepository<T> where T:class
+    {
+        //T will be Category for any other generic model on which we want to perform CRUD
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T,bool>> filter);
+        void Add(T entity);
+ 
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+
+
+    }
+}
