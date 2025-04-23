aad774889ae4f9eccbcf5d85bd953f1f3c0773c7
MalaghePayal <malaghepayal@gmail.com>
2025-04-23 21:38:27 +0530

    [feat repository] - Implement CategoryRepository with generic inheritance and specific interface
    - Created CategoryRepository class inheriting from Repository<Category> to utilize common CRUD operations
    - Implemented ICategoryRepository interface to define Category-specific methods: Update(Category obj) and Save()
    - Leveraged generic base class to promote code reuse and adhere to DRY principles
    - Ensured separation of concerns by handling generic operations in base class and specific operations in CategoryRepository
    - Enhanced maintainability and scalability by combining generic and specific repository patterns

---
+++ Myntra.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Myntra.DataAccess.Data;
+using Myntra.DataAccess.Repository.IRepository;
+using Myntra.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myntra.DataAccess.Repository
+{
+    /*Repository<Category> is a generic base class that provides common data access methods (like Add, GetAll, Remove, etc.) applicable to any entity type. By inheriting from this class, CategoryRepository gains these implementations, promoting code reuse.​
+
+ICategoryRepository is an interface that defines additional operations specific to the Category entity, such as Update and Save. By implementing this interface, CategoryRepository ensures it provides these specific functionalities.*/
+    public class CategoryRepository :Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext context):base(context)
+        {
+            _context = context;
+        }
+        public void Save()
+        {
+           _context.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _context.Categories.Update(obj);
+        }
+    }
+}
