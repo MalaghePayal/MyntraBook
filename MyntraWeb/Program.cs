fb2b91bea770fcc5e7d379419c40f38284662bf6
MalaghePayal <malaghepayal@gmail.com>
2025-04-23 22:04:35 +0530

    [fix] Resolve DI error by registering ICategoryRepository in Program.cs
    - Registered ICategoryRepository with its implementation CategoryRepository using AddScoped
    - Fixed 'Unable to resolve service for type ICategoryRepository' error in CategoryController
    - Ensured proper dependency injection setup for CategoryRepository

--- MyntraWeb/Program.cs
+++ MyntraWeb/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Myntra.DataAccess.Data;
+using Myntra.DataAccess.Repository;
+using Myntra.DataAccess.Repository.IRepository;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options=> options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
