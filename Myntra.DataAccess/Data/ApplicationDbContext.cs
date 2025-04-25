b7bb19aa4c0e03ce3d1569970a8987c0ce8e5496
MalaghePayal <malaghepayal@gmail.com>
2025-04-25 22:20:39 +0530

    [feat] Add Product DbSet and seed initial Product data
    - Added DbSet<Product> to ApplicationDbContext
    - Seeded Product entity with sample data using HasData
    - Included properties: Title, Description, ISBN, Author, tiered pricing

--- Myntra.DataAccess/Data/ApplicationDbContext.cs
+++ Myntra.DataAccess/Data/ApplicationDbContext.cs
@@ -10,7 +10,7 @@ namespace Myntra.DataAccess.Data
             
         }
         public DbSet<Category>  Categories { get; set; }
-
+        public DbSet<Product> Products { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Category>().HasData(
@@ -18,6 +18,11 @@ namespace Myntra.DataAccess.Data
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
                 new Category { Id = 3, Name = "History", DisplayOrder = 3 }
                 );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, Title = "Fortune of Time", Description = "XYZ", ISBN = "SWD9999001", Author = "Billy Spark", ListPrice = 99, Price1to50 = 90, Price50to100 = 85, Price100 = 80 });
         }
+
+        
     }
 }
