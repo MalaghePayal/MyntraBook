04988419ee9072c221a3e7c69e51b9fa6034fca3
MalaghePayal <malaghepayal@gmail.com>
2025-04-26 08:50:12 +0530

    [feat] Add Product DbSet and seed initial Product data
    - Added DbSet<Product> to ApplicationDbContext
    - Seeded Product entity with sample data using HasData
    - Included properties: Title, Description, ISBN, Author, tiered pricing

--- Myntra.DataAccess/Migrations/20250426031808_Product Table Added.Designer.cs
+++ Myntra.DataAccess/Migrations/20250426031808_Product Table Added.Designer.cs
@@ -2,17 +2,20 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Myntra.DataAccess.Data;
 
 #nullable disable
 
-namespace MyntraWeb.DataAccess.Migrations
+namespace Myntra.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250426031808_Product Table Added")]
+    partial class ProductTableAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
