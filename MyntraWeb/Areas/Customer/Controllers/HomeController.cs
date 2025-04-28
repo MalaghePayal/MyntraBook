d293268611a67d7a02ea1da25e70a1f1e3547950
MalaghePayal <malaghepayal@gmail.com>
2025-04-28 22:18:06 +0530

    [feat] - Modified Home Index View with Product Display
    - Replaced the old home page content with a new layout showing a list of products
    - Displayed product image, title, author, and pricing details in a responsive card format
    - Added a "Details" button for easy navigation to each product's page

--- MyntraWeb/Areas/Customer/Controllers/HomeController.cs
+++ MyntraWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Myntra.DataAccess.Repository.IRepository;
 using Myntra.Models;
 using System.Diagnostics;
 
@@ -7,15 +8,17 @@ namespace MyntraWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(ILogger<HomeController> logger,IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> ProductList = _unitOfWork.productRepository.GetAll(includeProperties:"category"); 
+            return View(ProductList);
         }
 
         public IActionResult Privacy()
