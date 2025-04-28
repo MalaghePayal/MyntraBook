6ce21e4fc32478f49c9b28b640f8003193b4cc9a
MalaghePayal <malaghepayal@gmail.com>
2025-04-28 13:27:12 +0530

    [feature] - Added product image upload functionality in Product Upsert
    - Created 'images/product' folder in wwwroot for storing uploaded product images
    - Injected IWebHostEnvironment in ProductController to access web root path
    - Updated Upsert POST action to handle image file upload and save it in 'images/product' directory
    - Assigned the uploaded image path to Product.ImageUrl
    - Ensured image is saved with a unique name using GUID
    - Updated view to support file uploads with enctype="multipart/form-data"
    - Added 'name' attribute in the file input field in Upsert view to handle file uploads

--- MyntraWeb/Areas/Admin/Controllers/ProductController.cs
+++ MyntraWeb/Areas/Admin/Controllers/ProductController.cs
@@ -13,10 +13,12 @@ namespace MyntraWeb.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment    _webHostEnvironment;
 
-        public ProductController(IUnitOfWork unitOfWork)
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -57,6 +59,19 @@ namespace MyntraWeb.Areas.Admin.Controllers
             
             if (ModelState.IsValid)
             {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (file!=null)
+                {
+                    string fileName = Guid.NewGuid().ToString()+ Path.GetExtension(file.FileName);    
+                    string ProductPath = Path.Combine(wwwRootPath, @"images\product");
+
+                    using(var fileStream = new FileStream(Path.Combine(ProductPath, fileName),FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    ProductVM.Product.ImageUrl = @"\images\product\" + fileName;
+                }
+
                 if (ProductVM.Product.Id ==0)
                 {
                     _unitOfWork.productRepository.Add(ProductVM.Product);
