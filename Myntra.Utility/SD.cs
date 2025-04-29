21a5db95208001905172c1c332a46a83d4541ba2
MalaghePayal <malaghepayal@gmail.com>
2025-04-29 22:27:23 +0530

    [feat] Add constants for User, Admin, and Company roles in SD class
    - Introduced constants for User, Admin, and Company roles in SD class
    - Simplified role management by referencing these constants throughout the code

--- Myntra.Utility/SD.cs
+++ Myntra.Utility/SD.cs
@@ -8,5 +8,9 @@ namespace Myntra.Utility
 {
     public static class SD //Static Details
     {
+        public const string Role_User_Customer = "Customer";
+        public const string Role_User_Company = "Company";
+        public const string Role_User_Admin = "Admin";
+        public const string Role_User_Employee = "Employee";
     }
 }
