fb3c50b96a4b1a437088f0e0e487ffb5a828c0c9
MalaghePayal <malaghepayal@gmail.com>
2025-05-01 11:09:44 +0530

    [feat] add ShoppingCartController with Index view and integrate cart link in _Layout
    - Created ShoppingCartController with [Area("Customer")] attribute.
    - Added Index view for displaying cart items.
    - Updated _Layout.cshtml to include a cart icon link pointing to ShoppingCart/Index.

---
+++ MyntraWeb/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyntraWeb.Areas.Customer.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        [Area("Customer")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
