1e2eb1738c3305bccc874a0e7d1d45ad36427709
MalaghePayal <malaghepayal@gmail.com>
2025-05-01 11:57:31 +0530

    [feat] show logged-in user's cart items in ShoppingCartController
    - Used IUnitOfWork to get cart items for the current user from the database.
    - Included related Product details while fetching the cart.
    - Used [Authorize] to make sure only logged-in users can access the cart page.

--- MyntraWeb/Areas/Customer/Controllers/ShoppingCartController.cs
+++ MyntraWeb/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -1,17 +1,39 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Myntra.DataAccess.Repository.IRepository;
+using Myntra.Models;
 using Myntra.Models.ViewModels;
+using System.Security.Claims;
 
 namespace MyntraWeb.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class ShoppingCartController : Controller
     {
-        [Area("Customer")]
+        
+        
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM shoppingCartVM { get; set; }
+
+        public ShoppingCartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            
+        }
+        
         [Authorize]
         public IActionResult Index()
         {
-            return View();
+            // Retrieve the current logged-in user's ID
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var UserId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            shoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.shoppingCartRepository.GetAll(u => u.ApplicationUserId == UserId,
+                includeProperties:"Product")
+            };
+            return View(shoppingCartVM);
         }
     }
 }
