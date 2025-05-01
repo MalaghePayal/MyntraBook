731998531a0c2892847fc0cf058fe43826c3122a
MalaghePayal <malaghepayal@gmail.com>
2025-05-01 22:34:43 +0530

    [feat] add OrderHeader and OrderDetail models to capture order and item-level data
    - Created `OrderHeader` class to store overall order information like user, shipping details, payment info, and status.
    - Created `OrderDetail` class to store individual product info for each order including quantity and price at time of purchase.
    - Added necessary data annotations for validation, relationships, and schema configuration.

--- Myntra.DataAccess/Migrations/20250501164943_AddOrderHeadersAndOrderDetailsToDb.Designer.cs
+++ Myntra.DataAccess/Migrations/20250501164943_AddOrderHeadersAndOrderDetailsToDb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Myntra.DataAccess.Data;
 
@@ -11,9 +12,11 @@ using Myntra.DataAccess.Data;
 namespace Myntra.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250501164943_AddOrderHeadersAndOrderDetailsToDb")]
+    partial class AddOrderHeadersAndOrderDetailsToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Myntra.Models/OrderDetail.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myntra.Models
+{
+    public class OrderDetail
+    {
+    public int Id { get; set; }
+     [Required]
+     public int OrderHeaderId { get; set; }
+        [ForeignKey(nameof(OrderHeaderId))]
+        [ValidateNever]
+        public OrderHeader OrderHeader { get; set; }
+        [Required]
+
+        public int ProductId { get; set; }
+        [ForeignKey(nameof(ProductId))]
+        [ValidateNever]
+        public Product Product { get; set; }
+
+        public int Count { get; set; }
+        //we added price here despite we already have it in product.
+        //because product price can change but we want here the pricec when user added
+        //that product to the cart.
+        public double Price { get; set; }
+    }
+}
