f7020f8c4ed3c444a9e465b4818bddce0bf31c08
MalaghePayal <malaghepayal@gmail.com>
2025-05-01 11:21:45 +0530

    [feat] add ShoppingCartVM and restrict ShoppingCartController Index action with [Authorize]
    - Added ShoppingCartVM to hold ShoppingCart list and total order value.
    - Applied [Authorize] attribute to Index action to restrict cart access to authenticated users.

--- MyntraWeb/Areas/Customer/Controllers/ShoppingCartController.cs
+++ MyntraWeb/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Myntra.DataAccess.Repository.IRepository;
+using Myntra.Models.ViewModels;
 
 namespace MyntraWeb.Areas.Customer.Controllers
 {
     public class ShoppingCartController : Controller
     {
         [Area("Customer")]
+        [Authorize]
         public IActionResult Index()
         {
             return View();
