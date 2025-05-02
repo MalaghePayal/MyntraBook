56e75dc27cb05684b43c21d684159d249195cdff
MalaghePayal <malaghepayal@gmail.com>
2025-05-02 13:27:48 +0530

    [feat] Add OrderController and OrderVM
    - Introduced OrderController to handle order views and actions
    - Added OrderVM view model to encapsulate OrderHeader and associated OrderDetails

---
+++ MyntraWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Myntra.DataAccess.Repository.IRepository;
+
+namespace MyntraWeb.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork =unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
